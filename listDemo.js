45d0bc58ab9e93e5d892048e5dae61ee25d46405
sam-klok <sklok11@gmail.com>
2024-10-09 21:28:31 -0700

    array modification

--- listDemo.js
+++ listDemo.js
@@ -3,7 +3,16 @@ var people = [
     { id: 2, name: "Tristan Klokov", dob: new Date("12/3/2010"), isStudent: true },
     { id: 3, name: "McKayla Klokov", dob: new Date("7/24/2012"), isStudent: true }
 ];
-var searchString = "Ser";
+var searchString = "Serge";
 var isNameFound = people.some(function (p) { return p.name.indexOf(searchString) > -1; });
+console.log("Search string " + searchString + " found in array: " + isNameFound);
+// let's create new array with names only
+var newList = people.map(function (p) { return p.name.substring(0, p.name.indexOf('Klokov') - 1); });
+var joinedString = newList.join("; ");
+console.log(joinedString);
+// .includes cause compile error:
+//error TS2550: Property 'includes' does not exist on type 'string[]'. Do you need to change your target library? 
+//Try changing the 'lib' compiler option to 'es2016' or later.ts(2550)
+isNameFound = newList.includes(searchString);
 console.log("Search string " + searchString + " found in array: " + isNameFound);
 //# sourceMappingURL=listDemo.js.map
--- listDemo.ts
+++ listDemo.ts
@@ -11,6 +11,18 @@ const people: Person[] = [
     {id:3,name:"McKayla Klokov",dob:new Date("7/24/2012"),isStudent:true}
 ]
 
-let searchString:string = "Ser";
+let searchString:string = "Serge";
 let isNameFound:boolean = people.some(p=>p.name.indexOf(searchString)>-1);
+console.log(`Search string ${searchString} found in array: ${isNameFound}`);
+
+// let's create new array with names only
+let newList:string[] = people.map(p=>p.name.substring(0,p.name.indexOf('Klokov')-1));
+
+const joinedString: string = newList.join("; "); 
+console.log(joinedString);
+
+// .includes cause compile error:
+//error TS2550: Property 'includes' does not exist on type 'string[]'. Do you need to change your target library? 
+//Try changing the 'lib' compiler option to 'es2016' or later.ts(2550)
+isNameFound = newList.includes(searchString);
 console.log(`Search string ${searchString} found in array: ${isNameFound}`);
